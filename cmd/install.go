c82cef85bee7ed975afff0a2db9e26045a67ae63
Shane Hanna <shane.hanna@gmail.com>
2014-08-11 23:51:56 +1000

    Move git twiddling into vendor package.

--- cmd/install.go
+++ cmd/install.go
@@ -1,11 +1,10 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/codegangsta/cli"
-	git "github.com/libgit2/git2go"
 	"github.com/shanna/git-vendor/vendor"
 	"log"
-	"os"
 )
 
 var CmdInstall = cli.Command{
@@ -19,26 +18,18 @@ var CmdInstall = cli.Command{
 }
 
 func runInstall(ctx *cli.Context) {
-	setup(ctx)
-
-	// TODO: The repo location stuff can probably be moved into vendor/* as well.
-
-	repo, err := git.OpenRepository(repoDirectory)
+	vc, err := vendor.Open(".")
 	if err != nil {
-		log.Fatalf("error reading git repository: %s", err.Error())
+		log.Fatalf(err.Error())
 	}
 
-	log.Printf("changing work directory to %s", repo.Workdir())
-	if err := os.Chdir(repo.Workdir()); err != nil {
-		log.Fatalf("failed to change work directory: ", err.Error())
+	if len(vc.Repositories) == 0 {
+		log.Fatalf("no " + vendor.Filename + ", nothing to vendor")
 	}
-
-	vc, err := vendor.Load(vendorFilename)
-	if err != nil {
-		log.Fatalf("failed to read config: %s", err.Error())
-	}
-
 	for _, repository := range vc.Repositories {
-		log.Printf("%+v", repository)
+		log.Printf("vendor " + repository.Name)
+		if err := repository.Vendor(); err != nil {
+			log.Fatalf(fmt.Sprintf("failed to vendor %s: %s", repository.Name, err.Error()))
+		}
 	}
 }
--- vendor/vendor.go
+++ vendor/vendor.go
@@ -4,11 +4,14 @@ import (
 	"errors"
 	"fmt"
 	git "github.com/libgit2/git2go"
+	"os"
 	"strings"
 )
 
+var Filename string = ".gitvendor"
+
 type Vendor struct {
-	Config       string
+	config       *git.Config
 	Repositories []*Repository
 }
 
@@ -19,23 +22,40 @@ type Repository struct {
 	Branch string
 }
 
-func Load(config string) (*Vendor, error) {
-	v := &Vendor{
-		Config:       config,
-		Repositories: make([]*Repository, 0),
+func discover(path string) (*git.Config, error) {
+	if path == "" || path == "." {
+		cwd, err := os.Getwd()
+		if err != nil {
+			return nil, errors.New("failed to get current work directory: " + err.Error())
+		}
+		path = cwd
 	}
 
-	parent, err := git.NewConfig()
+	path, err := git.Discover(path, true, []string{"/"})
 	if err != nil {
-		return nil, errors.New(fmt.Sprintf("error creating git config: %s", err.Error()))
+		return nil, errors.New("failed to get git work directory: " + err.Error())
 	}
 
-	vendor, err := git.OpenOndisk(parent, config)
+	repo, err := git.OpenRepository(path)
 	if err != nil {
-		return nil, errors.New(fmt.Sprintf("error reading config: %s", err.Error()))
+		return nil, errors.New("failed reading git repository: " + err.Error())
 	}
 
-	it, _ := vendor.NewIteratorGlob(`vendor\..*?\.url`)
+	if err := os.Chdir(repo.Workdir()); err != nil {
+		return nil, errors.New("failed to change work directory: " + err.Error())
+	}
+
+	parent, _ := git.NewConfig()
+	config, err := git.OpenOndisk(parent, Filename)
+	if err != nil {
+		return nil, errors.New("failed to read config: " + err.Error())
+	}
+	return config, nil
+}
+
+func repositories(config *git.Config) ([]*Repository, error) {
+	repos := make([]*Repository, 0)
+	it, _ := config.NewIteratorGlob(`vendor\..*?\.url`)
 	for {
 		entry, _ := it.Next()
 		if entry == nil {
@@ -43,20 +63,67 @@ func Load(config string) (*Vendor, error) {
 		}
 
 		parts := strings.Split(entry.Name, ".")
-		repository := &Repository{
-			Name: parts[1],
-			Url:  entry.Value,
+		repo := &Repository{
+			Name:   parts[1],
+			Url:    entry.Value,
+			Path:   parts[1],
+			Branch: "master",
 		}
+		if repoPath, err := config.LookupString(fmt.Sprintf("vendor.%s.path", repo.Name)); err == nil {
+			repo.Path = repoPath
+		}
+		if repoBranch, err := config.LookupString(fmt.Sprintf("vendor.%s.branch", repo.Name)); err == nil {
+			repo.Branch = repoBranch
+		}
+		repos = append(repos, repo)
+	}
+	return repos, nil
+}
 
-		if path, err := vendor.LookupString(fmt.Sprintf("vendor.%s.path", repository.Name)); err == nil {
-			repository.Path = path
-		}
-		if branch, err := vendor.LookupString(fmt.Sprintf("vendor.%s.branch", repository.Name)); err == nil {
-			repository.Branch = branch
-		}
-
-		v.Repositories = append(v.Repositories, repository)
+func Open(path string) (*Vendor, error) {
+	config, err := discover(path)
+	if err != nil {
+		return nil, err
 	}
 
-	return v, nil
+	repos, err := repositories(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Vendor{
+		config:       config,
+		Repositories: repos,
+	}, nil
+}
+
+func (r Repository) Vendor() error {
+	if stat, err := os.Stat(r.Path); os.IsNotExist(err) {
+		if err = os.MkdirAll(r.Path, 0776); err != nil {
+			return err
+		}
+
+		options := &git.CloneOptions{} // TODO: Progress and credentials?
+		if _, err := git.Clone(r.Url, r.Path, options); err != nil {
+			return err
+		}
+	} else if !stat.IsDir() {
+		return errors.New(fmt.Sprintf("%s exists, but is a file", r.Path))
+	}
+
+	repository, err := git.OpenRepository(r.Path)
+	if err != nil {
+		return err
+	}
+
+	remote, err := repository.LoadRemote("origin")
+	if err != nil {
+		return err
+	}
+
+	if err := remote.Fetch(nil, ""); err != nil {
+		return err
+	}
+
+	return nil
 }
